f2319128cabb83361c04dd330c9e59cf6fea2f91
Tanvir Ahmed <57531208+tanvir-tech@users.noreply.github.com>
2021-12-06 23:54:10 +0600

    Create loopNumDecSERIES.asm

---
+++ loopNumDecSERIES.asm
@@ -0,0 +1,21 @@
+.model small
+.stack 100h
+.data
+.code
+main proc
+    mov cx,5
+    
+    mov ah,2
+    mov dl,57
+    
+    l1:
+    int 21h
+    dec dl
+    dec dl
+    loop l1
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
