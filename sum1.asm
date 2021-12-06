9135e509f716e05ccac71cbc559bbde95c4f355d
Tanvir Ahmed <57531208+tanvir-tech@users.noreply.github.com>
2021-12-06 20:26:56 +0600

    Create sum1.asm

---
+++ sum1.asm
@@ -0,0 +1,30 @@
+.model small
+.stack 100h
+.code
+main proc
+    mov ah,1
+    int 21h
+    mov bh,al
+
+    mov ah,1
+    int 21h
+    mov bl,al
+
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+
+    add bh,bl
+    sub bh,48
+
+    mov ah,2
+    mov dl,bh
+    int 21h
+
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
